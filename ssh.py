01d07db674836da4f16722a8705c574a86f17dc6
KinReiBoku <kinreiboku.tomorrow@gmail.com>
2015-02-24 23:48:13 +0900

    add file

---
+++ ssh.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+import ssh
+
+# 接続情報
+HOST = '192.168.43.50'
+USER = 'default'
+PASS = 'password'
+PORT = 22
+
+# ssh接続
+ssh = ssh.SSHClient()
+ssh.load_system_host_keys()
+ssh.connect(HOST, username=USER, password=PASS, port=PORT)
+
+# コマンド実行
+stdin, stdout, stderr = ssh.exec_command('powershell .\\New-Folder.ps1')
+
+# 実行結果出力
+for line in stdout.read().split('\n'):
+	print line
+
+# 切断
+ssh.close()
---
+++ ssh_paramiko.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+import paramiko
+
+# 接続情報
+HOST = '192.168.43.50'
+USER = 'default'
+PASS = 'password'
+PORT = 22
+
+# ssh接続
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(HOST, username=USER, password=PASS, port=PORT)
+
+# コマンド実行
+stdin, stdout, stderr = ssh.exec_command('powershell .\\New-Folder.ps1')
+
+# 実行結果出力
+for line in stdout.read().split('\n'):
+	print line
+
+# 切断
+ssh.close()
